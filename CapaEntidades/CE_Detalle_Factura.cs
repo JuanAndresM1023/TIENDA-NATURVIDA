8201044d6e7c6ee15b11b936605045bf2c6f9235
JuanAndresM1023 <131668813+JuanAndresM1023@users.noreply.github.com>
2023-04-26 10:28:45 -0500

    Add files via upload

---
+++ CapaEntidades/CE_Detalle_Factura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class CE_Detalle_Factura
+    {
+        public int Numero_Factura { get; set; }
+
+        public int Cod_Productos { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public int Valor_Unidad { get; set; }
+    }
+}
---
+++ CapaEntidades/CE_Productos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+	public class CE_Productos
+	{
+		public int cod { get; set; }
+
+		public string descripcion { get; set; }
+
+		public int valorU { get; set; }
+
+		public int cantidad { get; set; }
+
+	}
+}
